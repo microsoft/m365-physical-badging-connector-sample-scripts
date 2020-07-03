794d2d9f9f309397d9781b27c5daa0ad4bc689ac
Onkar Singh <onkar.singh@microsoft.com>
2020-07-03 14:15:48 +0530

    U/onsing/add scripts for physical badging connector (#1)
    * Adding git ignore
    
    * Adding scripts to push org's physical badging signals
    
    * Adding details in Read me file.
    
    * Add json schema in README file
    
    * Update alignment of text in read me file
    
    * Fix alignment of sample json schema in read me
    
    * Renaming powershell script
    
    * Updating documentation link in read me
    
    Co-authored-by: onsing <onsing@microsoft.com>

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # Contributing
 
 This project welcomes contributions and suggestions.  Most contributions require you to agree to a
@@ -12,3 +11,86 @@ provided by the bot. You will only need to do this once across all repos using o
 This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
 For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
 contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+# m365-physical-badging-connector-sample-scripts
+This repository includes sample scripts for pushing organization's physical badging records, to be consumed by Microsoft's Insider Risk Management compliance solution. 
+
+This repository includes a powershell script, a postman script ans a sample file containing physical badging records. You can send up to 100K records per API call.
+
+## Creating AAD APP
+
+Create you AAD APP https://docs.microsoft.com/en-us/azure/kusto/management/access-control/how-to-provision-aad-app
+
+ Note the following
+ - APP_ID (aka Application ID or Client)
+ - APP_SECRET (aka client secret)
+ - TENANT_ID (aka directory ID)
+
+## Create your job on M365 Compliance portal
+
+Follow the steps in [Set up Physical Badging Connector](https://go.microsoft.com/fwlink/?linkid=2133516) to get started.
+Provide the required details, create an M365 Physical badging connector job and note the **JOB_ID**
+
+## Run the powershell script
+```powershell
+.\push_physical_badging_records.ps1 -tenantId "<Tenant Id>" -appId "<AAD App Id>" -appSecret "<AAD App Secret>" -jobId "<Job Id>" -jsonFilePath "<JSON_FILE_PATH>"
+```
+
+JSON_FILE_PATH must be the local file path for the json data file.
+
+If the last line reads **Upload Successful**, the script execution was successful.
+
+The script would retry  over a period of about 15 mins if it encounters any transient failures.
+
+## JSON Schema
+
+JSON schema below details all the fields to be sent as part of Physical badging records you will share with us. Please make sure you follow this as a reference while forming JSON payload. 
+
+Schema:
+```
+{
+	"title" : "Physical Badging Signals",
+	"description" : "Access signals from physical badging systems",
+	"DataType" : {
+		"description" : "Identify what is the data type for input signal",
+		"type" : "string",
+	},
+	"type" : "object",
+	"properties": {
+		"User UPN" : {
+			"description" : "Unique identifier AAD Id resolved by the source system",
+			"type" : "string",
+		},
+		"Asset ID": {
+			"description" : "Unique ID of the physical asset/access point",
+			"type" : "string",
+		},
+		"AssetName": {
+			"description" : "friendly name of the physical asset/access point",
+			"type" : "string",
+		},
+		"Time" : {
+			"description" : "timestamp of access",
+			"type" : "string",
+		},
+		"AccessStatus" : {
+			"description" : "what was the status of access attempt - Success/Failure",
+			"type" : "string",
+		},
+	}
+	"required" : ["User UPN", "Asset ID", "Time" "AccessStatus"]
+}
+```
+
+## Common Errors and resolution
+
+1. JOB_ID might be incorrect. Make sure it matches the one configured on M365 Compliance portal
+
+> RetryCommand : Failure with StatusCode [Forbidden] and ReasonPhrase [jobId and corresponding appId do not match.]
+> 
+
+2. APP_ID or TENANT_ID might be incorrect.
+
+> RetryCommand : {"error":"unauthorized_client","error_description":"AADSTS700016: Application with identifier '689412fa-4b24-475a-ab39-32eca848b6f2' was 
+> not found in the directory '85ee0691-54d7-49f1-b879-3ce53c2a8549'. This can happen if the application has not been installed by the administrator of the 
+> tenant or consented to by any user in the tenant. You may have sent your authentication request to the wrong tenant.}
---
+++ push_physical_badging_records.ps1
@@ -0,0 +1,145 @@
+# Usage: .\push_physical_badging_records.ps1 -tenantId "<Tenant Id>" -appId "<AAD App Id>" -appSecret "<AAD App Secret>" -jobId "<Job Id>" -jsonFilePath "<JSON_FILE_PATH>"
+
+param
+(   
+    [Parameter(mandatory = $true)]
+    [string] $tenantId,
+    [Parameter(mandatory = $true)]
+    [string] $appId,
+    [Parameter(mandatory = $true)]
+    [string] $appSecret,
+    [Parameter(mandatory = $true)]
+    [string] $jobId,
+    [Parameter(mandatory = $true)]
+    [string] $jsonFilePath
+)
+
+# Access Token Config
+$oAuthTokenEndpoint = "https://login.windows.net/$tenantId/oauth2/token"
+$resource = 'https://microsoft.onmicrosoft.com/4e476d41-2395-42be-89ff-34cb9186a1ac'
+
+# End point config
+$eventApiUrl = "https://webhook.ingestion.office.com"
+$eventApiEndpoint = "api/signals/physicalbadging"
+
+function GetAccessToken () {
+	Write-Host -fore green "******Getting Access Token******"
+    # Token Authorization URI
+    $uri = "$($oAuthTokenEndpoint)?api-version=1.0"
+
+    # Access Token Body
+    $formData = 
+    @{
+        client_id     = $appId;
+        client_secret = $appSecret;
+        grant_type    = 'client_credentials';
+        resource      = $resource;
+        tenant_id     = $tenantId;
+    }
+
+    # Parameters for Access Token call
+    $params = 
+    @{
+        URI         = $uri
+        Method      = 'Post'
+        ContentType = 'application/x-www-form-urlencoded'
+        Body        = $formData
+    }
+
+    $response = Invoke-RestMethod @params -ErrorAction Stop
+	Write-Host -fore green "******Access Token Acquired******"
+    return $response.access_token
+}
+
+function RetryCommand {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position = 0, Mandatory = $true)]
+        [scriptblock]$ScriptBlock,
+
+        [Parameter(Position = 1, Mandatory = $false)]
+        [int]$Maximum = 15
+    )
+
+    Begin {
+        $cnt = 0
+    }
+
+    Process {
+        do {
+            $cnt++
+            try {
+                $ScriptBlock.Invoke()
+                return
+            }
+            catch {
+                Write-Error $_.Exception.InnerException.Message -ErrorAction Continue
+                Start-Sleep 60
+                if ($cnt -lt $Maximum) {
+                    Write-Output "Retrying"
+                }
+            }
+            
+        } while ($cnt -lt $Maximum)
+
+        throw 'Execution failed.'
+    }
+}
+
+function WriteErrorMessage($errorMessage) {
+    $Exception = [Exception]::new($errorMessage)
+    $ErrorRecord = [System.Management.Automation.ErrorRecord]::new(
+        $Exception,
+        "errorID",
+        [System.Management.Automation.ErrorCategory]::NotSpecified,
+        $TargetObject
+    )
+    $PSCmdlet.WriteError($ErrorRecord)
+}
+
+function PushPhysicalBadgingRecords ($access_token) {
+    $nvCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty) 
+    $nvCollection.Add('jobid', $jobId)
+    $uriRequest = [System.UriBuilder]"$eventApiUrl/$eventApiEndpoint"
+    $uriRequest.Query = $nvCollection.ToString()
+
+    $url = $uriRequest.Uri.OriginalString
+
+    try{
+		$json = Get-Content $jsonFilePath
+    }catch{
+        WriteErrorMessage("Error reading from file. Please check if path is correct")
+        return
+    }
+	
+	$headers = @{
+		"Accept"="application/json"
+		"Authorization"="Bearer $access_token"
+		"Content-Type"="application/json"
+	} 
+	
+    try {
+		$result = Invoke-WebRequest -Uri $url -Method POST -Body $json -Headers $headers -TimeoutSec 150
+    }
+    catch {
+		WriteErrorMessage($_)
+        return
+    }
+
+	$status_code = [int]$result.StatusCode
+    if ($status_code -eq 200 -or $status_code -eq 201) {
+		Write-Host -fore green "******Upload Successful******"
+		Write-Output $result.Content
+    }
+    elseif ($status_code -eq 0 -or $status_code -eq 501 -or $status_code -eq 503) {
+        throw "Service unavailable."
+    }
+    else {
+        WriteErrorMessage("Failure with StatusCode [{0}] and ReasonPhrase [{1}]" -f $result.StatusCode, $result.ReasonPhrase)
+    }
+}
+
+RetryCommand -ScriptBlock {
+    $access_token = GetAccessToken
+    PushPhysicalBadgingRecords($access_token)
+}
